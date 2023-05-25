4cbf3312496d8fa06e296f35c49815ea27330374
malcomowilla <owillamalcom@gmail.com>
2023-05-25 14:50:18 +0300

    s

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -3,13 +3,21 @@ import { useParams } from "react-router-dom";
 
 function MovieShow({ movies }) {
   // call useParams to access the `params` from the url
-  const params = useParams();
-  console.log(params);
+const params = useParams()
+console.log(params)
+  console.log(movies);
+
+if (!movies[params.movieId]) {
+  return <h1> movie not found</h1>
+}
+
   return (
     <div>
       {/* And here we access the `movieId` stored in `params` to render 
         information about the selected movie */}
-      <h3>{movies[params.movieId].title}</h3>
+
+        <h3>{movies[params.movieId].title}</h3>
+
     </div>
   );
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 
 function MoviesList({ movies }) {
  const renderMovies = Object.keys(movies).map((movieID)=>(
-  <li key={movieID}>
-    <Link to={`/movies/${movieID}`} >
-    {movies[movieID].title}</Link>
-  </li>
+
+  <li key={movieID}> <Link to={`/movies/${movieID}`}>{movies[movieID].title}</Link></li>
  ))
 
+//  const renderMovies = Object.keys(movies).map((movieID)=>{
+//   const movie = movies[movieID]
+//   return (
+// <li key={movieID}><Link to={`/movies/${movieID}`}>{movie.title}</Link></li>
+//   )
+//  })
 
  return <ul>{renderMovies}</ul>
 }
